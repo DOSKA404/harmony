61a55164a3557eaf5b2ecd8605de0152d564d745
Sweefty <rfsilver@hotmail.fr>
2024-04-29 23:51:53 +0200

    adding login and signup

--- frontend/harmony/src/app/Register/page.tsx
+++ frontend/harmony/src/app/Register/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import { Layout } from '../../components/layout';
 import {
   Box,
@@ -15,49 +15,44 @@ import {
   Link
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { signIn } from 'next-auth/react';
+
+
+interface InputStyleProps {
+  variant: string;
+  size: string;
+  focusBorderColor: string;
+  _placeholder: { color: string };
+}
 
 export default function Register() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
 
-  const inputStyle = {
+  const inputStyle: InputStyleProps = {
     variant: 'flushed',
     size: 'lg',
     focusBorderColor: '#47A8BD',
     _placeholder: { color: 'rgba(255, 255, 255, 0.7)' }
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
+    const data = new FormData(e.target);
 
-    try {
-      const response = await fetch('/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-          username,
-          password,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to register');
-      }
-
-      const data = await response.json();
-      setMessage(`Registered with username: ${data.username} and ID: ${data.id}`);
-    } catch (error) {
-      console.error(error);
-      setMessage('Failed to register');
-    }
+    await signIn('signup', {
+      username: data.get('username') as string,
+      password: data.get('password') as string,
+      redirect: true,
+      callbackUrl: "/Home"
+    });
   };
+  
 
   return (
     <Layout>
-      <Center w="full" h="100vh" bg="black">
+      <Center w="full" h="100vh" bg="#0A0F12">
         <Box p={8} w={['90%', '70%', '50%', '40%']} maxWidth="600px" borderWidth="1px" borderRadius="lg" boxShadow="lg" bgGradient="linear(to-r, #333333, #1a1a1a)">
           <form onSubmit={handleSubmit}>
             <VStack spacing={8} align="center">
@@ -68,6 +63,7 @@ export default function Register() {
                 <FormLabel htmlFor="username" color="white" fontSize="lg">Pseudo</FormLabel>
                 <Input
                   id="username"
+                  name='username'
                   placeholder="Entrez votre pseudo"
                   {...inputStyle}
                   value={username}
@@ -78,6 +74,7 @@ export default function Register() {
                 <FormLabel htmlFor="password" color="white" fontSize="lg">Mot de passe</FormLabel>
                 <Input
                   id="password"
+                  name='password'
                   placeholder="Entrez votre mot de passe"
                   type="password"
                   {...inputStyle}
--- frontend/harmony/src/app/page.tsx
+++ frontend/harmony/src/app/page.tsx
@@ -1,52 +1,109 @@
-// Home.tsx
+'use client'
+import { useState } from 'react';
 import { Layout } from '../components/layout';
-import { Box, Flex, Heading, Text, useColorModeValue, SimpleGrid, Image } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Heading,
+  Center,
+  VStack,
+  Text,
+  Link
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
-import { ChakraProvider } from '@chakra-ui/react';
-import Head from 'next/head';
+import { signIn } from 'next-auth/react';
 
-function Home() {
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const inputStyle = {
+    variant: 'flushed',
+    size: 'lg',
+    focusBorderColor: '#47A8BD',
+    _placeholder: { color: 'rgba(255, 255, 255, 0.7)' }
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    await signIn('login', {
+      username: data.get('username') as string,
+      password: data.get('password') as string,
+      redirect: true,
+      callbackUrl: "/Home"
+    });
+  };
   
+
   return (
     <Layout>
-      <Box w="full" h="100vh" bg="black">
-        <Flex
-          direction="column"
-          h="full"
-          w="full"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Heading mb={10} color="white" fontSize="4xl">
-          The music in your skin
-          </Heading>
-          <SimpleGrid columns={3} spacing={10} w="full" px={10} py={5}>
-            <Box bgGradient="linear(to-r, #333333, #1a1a1a)" p={10} borderRadius="lg" boxShadow="xl">
-              <Heading mb={2} size="md" color="white">Artiste</Heading>
-              <Text color="whiteAlpha.800">Découvrez des artistes de tous genres et horizons.</Text>
-              <NextLink href="/artists" passHref>
-                <Text mt={4} as="a" color="teal.400" cursor="pointer">Explorer les artistes</Text>
-              </NextLink>
-            </Box>
-            <Box bgGradient="linear(to-r, #333333, #1a1a1a)" p={10} borderRadius="lg" boxShadow="xl">
-              <Heading mb={2} size="md" color="white">Musique</Heading>
-              <Text color="whiteAlpha.800">Parcourez notre vaste bibliothèque musicale.</Text>
-              <NextLink href="/music" passHref>
-                <Text mt={4} as="a" color="teal.400" cursor="pointer">Écouter maintenant</Text>
-              </NextLink>
-            </Box>
-            <Box bgGradient="linear(to-r, #333333, #1a1a1a)" p={10} borderRadius="lg" boxShadow="xl">
-              <Heading mb={2} size="md" color="white">Album</Heading>
-              <Text color="whiteAlpha.800">Trouvez les derniers albums de vos artistes préférés.</Text>
-              <NextLink href="/albums" passHref>
-                <Text mt={4} as="a" color="teal.400" cursor="pointer">Voir les albums</Text>
-              </NextLink>
-            </Box>
-          </SimpleGrid>
-        </Flex>
-      </Box>
+      <Center w="full" h="100vh" bg="#0A0F12">
+        <Box p={8} w={['90%', '70%', '50%', '40%']} maxWidth="600px" borderWidth="1px" borderRadius="lg" boxShadow="lg" bgGradient="linear(to-r, #333333, #1a1a1a)">
+          <form onSubmit={handleSubmit}>
+            <VStack spacing={8} align="center">
+              <Heading as="h1" size="xl" color="white">
+                Connexion
+              </Heading>
+              <FormControl isRequired>
+                <FormLabel htmlFor="username" color="white" fontSize="lg">Pseudo</FormLabel>
+                <Input
+                  id="username"
+                  name='username'
+                  placeholder="Entrez votre pseudo"
+                  {...inputStyle}
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel htmlFor="password" color="white" fontSize="lg">Mot de passe</FormLabel>
+                <Input
+                  id="password"
+                  name='password'
+                  placeholder="Entrez votre mot de passe"
+                  type="password"
+                  {...inputStyle}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                size="lg"
+                backgroundColor="teal.700"  // Utilisation d'une couleur plus foncée
+                color="white"
+                _hover={{
+                  backgroundColor: "teal.800",  // Assombrissement lors du survol
+                  transform: 'scale(1.03)'  // Légère augmentation de la taille pour l'interaction
+                }}
+                _active={{
+                  backgroundColor: "teal.900"  // Encore plus sombre lors du clic
+                }}
+                mt={6}
+                px={10}
+              >
+                Se connecter
+              </Button>
+
+
+              <Flex mt={4} align="center" justify="center">
+                <Text color="whiteAlpha.800" mr={2}>Pas de compte ?</Text>
+                <NextLink href="/Register" passHref>
+                  <Link color="teal.400">Inscription</Link>
+                </NextLink>
+              </Flex>
+              {message && <Text color="red.500" mt={4}>{message}</Text>}
+            </VStack>
+          </form>
+        </Box>
+      </Center>
     </Layout>
   );
 }
-
-export default Home;
